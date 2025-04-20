c27d8c76c04c70f5b29247ad9de33da66092bcb9
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-20 13:49:10 -0500

    Complete day 2
    day 2 part 1 - data types
    
    day 2 part 2 - type errors
    
    day 2 part 3 - operations
    
    day 2 part 4 manipulation
    
    day 2 part 5 - complete tip calculator project

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,19 @@
-len("Hello")
+# subscripting
+print("Hello"[-1])
+
+# string
+print(("123" + "345"))
+
+# Integer = Whole Number
+print(123 + 345)
+
+# Large Integers
+print(123_345_6789)
+
+# Float = floating point number
+print(3223.33333)
+
+# Boolean
+print(True)
+print(False)
+
--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,21 @@
-bmi = 84 / 1.65 ** 2
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+#
+# print(int(bmi))
+#
+# #  flooring
+# print(round(bmi))
+#
+# print(round(bmi, 2))
+
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"your score is = {score}, your height = {height} you are winning is {is_winning}")
+
+# score -= 1
+# print(score)
+#
+# #  f-strings
+# print("your score is " + str(score))
