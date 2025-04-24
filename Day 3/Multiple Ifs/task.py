4aa8a1957d587bcb5e5a73a8c4ac910aceea802b
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-23 23:47:50 -0500

    Day 3
    update: curriculum for day 3
    
    add: part 1
    
    add: part 2
    
    add: part 4
    
    add: part 3
    
    add part 5
    
    add: pizza party challenge
    
    complete: treasure hunt adventure project

--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -1,14 +1,23 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-
+bill = 0
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        bill = 5
+        print("child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("adult tickets are $12.")
+
+    wants_photo = input("do you want a want a photo taken? its 3 dollars. type y for yes and n for no.")
+    if wants_photo == "y":
+        bill += 3
+
+    print(f"your final bill is {bill}")
 else:
     print("Sorry you have to grow taller before you can ride.")
