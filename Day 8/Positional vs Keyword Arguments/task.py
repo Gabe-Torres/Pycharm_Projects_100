a96b8759aeed7537fabba12757c669be9cb64ea6
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-05-03 20:08:42 -0500

    step 2

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,16 @@
 # Functions with input
+#
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
 
-def greet_with_name(name):
+def greet_with(name, local):
     print(f"Hello {name}")
-    print(f"How do you do {name}?")
+    print(f"what is it like in {local}")
 
+# greet_with("gabe", "texas")
 
-greet_with_name("Jack Bauer")
+greet_with(name = "gabe", local = "texas")
