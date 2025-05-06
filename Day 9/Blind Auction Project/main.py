49c7d0c09eb68ab20a2d22c8284d8e375ea0b2c4
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-05-06 18:10:33 -0500

    step 3 complete auction project

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,5 +2,33 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+from art import logo
+print(logo)
 
+data = {}
+continue_bid = True
+
+def highest_bidder(data):
+    high_bid = 0
+    winner = ''
+
+    for user in data:
+        bid = data[user]
+        if bid > high_bid:
+            high_bid = bid
+            winner = user
+    print(f"The winner is {winner} with a bid of ${high_bid}")
+
+
+while continue_bid:
+    user_input = input("What is your name? ")
+    user_bid = int(input("Whats is your bid? "))
+    data[user_input] = user_bid
+    question = input("Any other bidders? Type 'yes' or 'no'.\n")
+
+    if question == 'no':
+        continue_bid = False
+        highest_bidder(data)
+    elif question == 'yes':
+        print("\n" * 20)
 
